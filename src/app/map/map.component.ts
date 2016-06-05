cba87ab8925526cbe81e379da74508703142f073
culli <jim.cullison@gmail.com>
2016-06-05 09:02:24 -0600

    fixed usage of a timer to wait for svg elements to be available on the dom, new solution may be just another hack, or it may be the only way to do it with angular

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import {ViewChild, ElementRef, Component, Input, Inject} from '@angular/core';
+import {ViewChild, ElementRef, Component, Input, Inject, AfterViewChecked} from '@angular/core';
 import {OnActivate, Router, RouteSegment} from '@angular/router';
 
 import {Room} from '../room';
@@ -14,50 +14,75 @@ declare var jQuery:any;
   styleUrls: ['app/map/map.component.css'],
   directives: [SvgImageComponent]
 })
-export class MapComponent implements OnActivate {
+export class MapComponent implements OnActivate, AfterViewChecked {
 
   private image:string;
-  private site:string;
+  private site:Site;
+  private startDate:Date;
+  private endDate:Date;
+  private sitePromise:Promise<any>;
+  private roomCssInitialized:boolean = false
 
-
-  @ViewChild(SvgImageComponent) svg:ElementRef;
+  @ViewChild(SvgImageComponent) svg:SvgImageComponent;
 
   rooms:Room[] = [];
 
   constructor(private router:Router,
               private roomService:RoomService,
               private elementRef:ElementRef) {
+    this.startDate = new Date()
+    this.endDate = new Date()
+    this.endDate.setDate(this.endDate.getDate() + 7)
+
   }
 
   routerOnActivate(curr:RouteSegment):void {
-    this.site = curr.getParam('site');
-    this.roomService.getSite(this.site)
+    let siteName = curr.getParam('site');
+    this.sitePromise = this.roomService.getSite(siteName)
       .then((site:Site) => {
+        this.site = site;
         this.image = "assets/img/" + site.image;
         this.rooms = site.rooms;
       })
       .catch((err:any) => console.error(err))
-
   }
 
   showDetail(room:Room) {
     //TODO:
   }
 
-  getFreeBusy() {
-    this.roomService.getRoomAvailabilityTest()
+  getSiteAvailabilty() {
+    this.roomService.getSiteAvailability(this.site.name, this.startDate, this.endDate);
   }
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.rooms.forEach((room) => {
-        let roomElement = document.getElementById(room.id)
-        // console.log('room', room.id, roomElement)
-        if (roomElement) {
-          roomElement.setAttribute('style', '')
-          roomElement.setAttribute('class', 'conf-room')
-        }
-      })
-    }, 1000) //hack
+    // console.log('afterViewInit')
+  }
+
+  ngAfterViewChecked() {
+    // console.log('afterViewChecked')
+    this.svg.svgLoaded$.subscribe((val) => {
+      // console.log('svgLoaded event fired')
+      if (!this.roomCssInitialized) {
+        // console.log('initialized was false')
+        this.initRoomCss()
+      }
+    })
+  }
+
+  /*
+   inkscape doesn't have a good way to set css style/class, but at least it has a way to set element id
+   go through and find rooms by id and add css class
+   */
+  initRoomCss() {
+    this.rooms.forEach((room) => {
+      let roomElement = document.getElementById(room.id)
+      // console.log('room', room.id, roomElement)
+      if (roomElement) {
+        this.roomCssInitialized = true
+        roomElement.setAttribute('style', '')
+        roomElement.setAttribute('class', 'conf-room')
+      }
+    })
   }
 }
